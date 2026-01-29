24635b0fe11707559e5157ce20f980d99410ecc8
Amos Dorceus <amosdorceus2010@gmail.com>
2026-01-29 14:59:44 +0100

    MP-5 Configurer PostgreSQL

---
+++ src/conf/db/database.py
@@ -0,0 +1,45 @@
+from sqlmodel import SQLModel, create_engine, Session
+from src.settings import settings
+
+class Database:
+    _engine = None
+
+    @classmethod
+    def get_engine(cls):
+        if cls._engine is None:
+            cls._engine = create_engine(
+                f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}"
+                f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}",
+                echo=True
+            )
+        return cls._engine
+
+    @classmethod
+    def get_session(cls):
+        engine = cls.get_engine()
+        with Session(engine) as session:
+            yield session
+
+    @classmethod
+    def init_db(cls):
+       #import model import ici
+        from src.models import __all__ as models  
+
+        engine = cls.get_engine()
+        SQLModel.metadata.create_all(engine)
+
+    @classmethod
+    def _recreate_db(cls):
+        """
+        ⚠️ DANGEREUX – réservé aux scripts d’administration
+        """
+        engine = cls.get_engine()
+        SQLModel.metadata.drop_all(engine)
+        SQLModel.metadata.create_all(engine)
+
+
+    @classmethod
+    def disconnect(cls):
+        if cls._engine:
+            cls._engine.dispose()
+            cls._engine = None
